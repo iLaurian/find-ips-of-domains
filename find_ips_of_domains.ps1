14229170e7c4702f545312376c3b5e5aa57f3180
iLaurian <91423981+iLaurian@users.noreply.github.com>
2022-04-23 08:43:59 +0300

    Add files via upload

---
+++ find_ips_of_domains.ps1
@@ -0,0 +1,23 @@
+﻿###you'll need to change the path for the get-content and export-csv locations
+
+$result=@()
+Get-Content C:\Users\_____\Desktop\domainslist.csv | %{
+$start_name = $_
+
+$conn = Test-Connection $_ -count 1 -ErrorAction SilentlyContinue
+
+if($conn)
+{
+  $dns_ip = ($conn.IPV4Address).IPAddressToString
+}
+else
+{
+  $dns_ip = $null
+}
+$HostObj = New-Object PSObject -Property @{
+									Domain    = $start_name
+		        					IP      = $dns_ip   
+		        		}
+$result += $HostObj						
+}
+$result | Export-Csv C:\Users\______\Desktop\result.csv -NoTypeInformation
